dd0cfcdeff4e595a74339080d817b15bafa9ce41
Jillian Rowe <jillian.e.rowe@gmail.com>
2020-03-27 12:42:23 +0300

    WHY

--- .circleci/install_ci_deps.sh
+++ .circleci/install_ci_deps.sh
@@ -28,6 +28,4 @@ chmod 700 get_helm.sh
 curl -o aws-iam-authenticator \
     https://amazon-eks.s3-us-west-2.amazonaws.com/1.12.7/2019-03-27/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
-
 mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
-
